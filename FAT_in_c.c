b18df194163d448fd9d08bbecc18f168a9c96007
John Solsma <solsma@me.com>
2014-11-20 23:08:23 -0600

    Added notes + disk size

--- FAT_in_c.c
+++ FAT_in_c.c
@@ -10,8 +10,10 @@ int formatDisk(char *diskName, __int16_t sectorSize, __int16_t clusterSize, __in
 	    __int16_t x[10] = {1,2,3,4,5,6,5000,6,-10,11};
 	    __int16_t result[10];
 	    int i;
-	__int16_t sector[1]={sectorSize}; 
-	__int16_t cluster[1]={clusterSize};
+	__int16_t sector[1]   = {sectorSize};      // size of a sector in bytes
+	__int16_t cluster[1]   = {clusterSize};     // size of a cluster in sectors
+	__int16_t disk[2]       = {diskSize,0};     // size of disk in clusters
+	
 	//__int16_t sector=sectorSize;
 	// __int16_t = 4 bytes -> can store sector size + cluster size
 	// __int16_t = first 2 bytes -> store disk size
@@ -29,8 +31,9 @@ int formatDisk(char *diskName, __int16_t sectorSize, __int16_t clusterSize, __in
 
 	    if(fp != NULL)
 	    {
-		  fwrite(sector, sizeof(__int16_t), 1 /*20/2*/, fp);
+		fwrite(sector, sizeof(__int16_t), 1 /*20/2*/, fp);
 		fwrite(cluster, sizeof(__int16_t), 1 /*20/2*/, fp);
+		fwrite(disk, sizeof(__int16_t), 2 /*20/2*/, fp); // write disk size then 0 to next byte
 	        fwrite(x, sizeof(__int16_t), 10 /*20/2*/, fp); // write array to file in chunks the size of short (int16)
 		 // fwrite(z, sizeof(__int16_t), 1000000 /*20/2*/, fp); // write array to file in chunks the size of short (int16)
 
