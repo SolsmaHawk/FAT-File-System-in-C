41698de164fb06b830f21aeb1f6dd07fc0f4fe35
John Solsma <solsma@me.com>
2014-11-21 22:05:30 -0600

    Add info to disk readout

--- FAT_in_c.c
+++ FAT_in_c.c
@@ -1,6 +1,8 @@
+// John Solsma 2014
 #include <stdio.h>
 #include <time.h>
 #include <string.h>
+
 
 int initDisk(FILE *fileToInit, char *diskName)
 {
@@ -14,18 +16,19 @@ int initDisk(FILE *fileToInit, char *diskName)
 		return 0;
 }
 
+
 int formatDisk(FILE *fileToFormat, char *diskName, __int16_t sectorSize, __int16_t clusterSize, __int16_t diskSize, __int16_t fatStart, __int16_t fatLength, __int16_t dataStart, __int16_t dataLength)
 {
 	FILE *fp = fileToFormat;
 
 
-	__int16_t sector[1]   = {sectorSize};         // size of a sector in bytes
-	__int16_t cluster[1]   = {clusterSize};        // size of a cluster in sectors
-	__int16_t disk[1]       = {diskSize};            // size of disk in clusters
-	__int16_t fatS[1]       = {fatStart};            // start of the FAT
-	__int16_t fatL[1]       = {fatLength};         // length of the FAT
-	__int16_t dataS[1]    = {dataStart};         // start of the data
-	__int16_t dataL[1]    = {dataLength};       // length of the data
+	__int16_t sector[1]    = {sectorSize};       // size of a sector in bytes
+	__int16_t cluster[1]   = {clusterSize};      // size of a cluster in sectors
+	__int16_t disk[1]      = {diskSize};         // size of disk in clusters
+	__int16_t fatS[1]      = {fatStart};         // start of the FAT
+	__int16_t fatL[1]      = {fatLength};        // length of the FAT
+	__int16_t dataS[1]     = {dataStart};        // start of the data
+	__int16_t dataL[1]     = {dataLength};       // length of the data
 
 
 
@@ -33,14 +36,14 @@ int formatDisk(FILE *fileToFormat, char *diskName, __int16_t sectorSize, __int16
 
 	    if(fp != NULL)
 	    	{
-			fwrite(sector, sizeof(__int16_t), 1 /*20/2*/, fp); //0
-			fwrite(cluster, sizeof(__int16_t), 1 /*20/2*/, fp); //1
-			fwrite(disk, sizeof(__int16_t), 1 /*20/2*/, fp); //2
-			fwrite(fatS, sizeof(__int16_t), 1 /*20/2*/, fp);  //3
-			fwrite(fatL, sizeof(__int16_t), 1 /*20/2*/, fp); //4
-			fwrite(dataS, sizeof(__int16_t), 1 /*20/2*/, fp);  //5
-			fwrite(dataL, sizeof(__int16_t), 1 /*20/2*/, fp); //6
-			fwrite(diskName, sizeof(char[32]), 1 /*20/2*/, fp); //7 - Disk Name
+			fwrite(sector, sizeof(__int16_t), 1 /*20/2*/, fp);   //0
+			fwrite(cluster, sizeof(__int16_t), 1 /*20/2*/, fp);  //1
+			fwrite(disk, sizeof(__int16_t), 1 /*20/2*/, fp);     //2
+			fwrite(fatS, sizeof(__int16_t), 1 /*20/2*/, fp);     //3
+			fwrite(fatL, sizeof(__int16_t), 1 /*20/2*/, fp);     //4
+			fwrite(dataS, sizeof(__int16_t), 1 /*20/2*/, fp);    //5
+			fwrite(dataL, sizeof(__int16_t), 1 /*20/2*/, fp);    //6
+			fwrite(diskName, sizeof(char[32]), 1 /*20/2*/, fp);  //7 - Disk Name
 	    	}
 	   else
 		{
@@ -51,9 +54,10 @@ int formatDisk(FILE *fileToFormat, char *diskName, __int16_t sectorSize, __int16
 	return 0;
 }
 
+
 void readDisk(FILE *fileToRead, char *diskArea, char *diskName)
 {	int i;
-    	__int16_t result[10];
+    __int16_t result[10];
 	char result2[32];
 	FILE *fp = fileToRead;
 	if(strncmp("MBR",diskArea, 3)==0)
@@ -67,7 +71,29 @@ void readDisk(FILE *fileToRead, char *diskArea, char *diskName)
 				printf("\n");
 				for (i = 0; i < 7; i++)
 					{
-					printf("%d = %d\n", i, (int)result[i]);
+						switch ( i ) {
+						case 0:
+						printf("Sector size in bytes          = %d\n", (int)result[i]);
+						break;
+						case 1:
+						printf("Size of cluster in sectors    = %d\n", (int)result[i]);
+						break;
+						case 2:
+						printf("Size of disk in clusters      = %d\n", (int)result[i]);
+						break;
+						case 3:
+						printf("Start of FAT                  = %d\n", (int)result[i]);
+						break;
+						case 4:
+						printf("Length of FAT                 = %d\n", (int)result[i]);
+						break;
+						case 5:
+						printf("Start of data                 = %d\n", (int)result[i]);
+						break;
+						case 6:
+						printf("Length of data                = %d\n", (int)result[i]);
+						break;
+						}
 					}
 					printf("%s",result2);
 				fclose(fp);
